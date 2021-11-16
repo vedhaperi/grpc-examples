f53c6b44705bc7e181a4fce4951c1457ed066e17
Ubuntu <azureuser@jacques.0lyzaa4cvkge3nliary1atabxc.xx.internal.cloudapp.net>
2021-11-16 01:55:33 +0000

    add backup rpc

--- helloworld/helloworld_grpc.pb.go
+++ helloworld/helloworld_grpc.pb.go
@@ -22,6 +22,8 @@ type GreeterClient interface {
 	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
 	// Check heartbeat
 	CheckHeartbeat(ctx context.Context, in *BeatRequest, opts ...grpc.CallOption) (*BeatReply, error)
+	// Sends backup to other master
+	SendBackup(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
 }
 
 type greeterClient struct {
@@ -50,6 +52,15 @@ func (c *greeterClient) CheckHeartbeat(ctx context.Context, in *BeatRequest, opt
 	return out, nil
 }
 
+func (c *greeterClient) SendBackup(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error) {
+	out := new(HelloReply)
+	err := c.cc.Invoke(ctx, "/helloworld.Greeter/SendBackup", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GreeterServer is the server API for Greeter service.
 // All implementations must embed UnimplementedGreeterServer
 // for forward compatibility
@@ -58,6 +69,8 @@ type GreeterServer interface {
 	SayHello(context.Context, *HelloRequest) (*HelloReply, error)
 	// Check heartbeat
 	CheckHeartbeat(context.Context, *BeatRequest) (*BeatReply, error)
+	// Sends backup to other master
+	SendBackup(context.Context, *HelloRequest) (*HelloReply, error)
 	mustEmbedUnimplementedGreeterServer()
 }
 
@@ -70,6 +83,9 @@ func (UnimplementedGreeterServer) SayHello(context.Context, *HelloRequest) (*Hel
 }
 func (UnimplementedGreeterServer) CheckHeartbeat(context.Context, *BeatRequest) (*BeatReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckHeartbeat not implemented")
+}
+func (UnimplementedGreeterServer) SendBackup(context.Context, *HelloRequest) (*HelloReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SendBackup not implemented")
 }
 func (UnimplementedGreeterServer) mustEmbedUnimplementedGreeterServer() {}
 
@@ -120,6 +136,24 @@ func _Greeter_CheckHeartbeat_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Greeter_SendBackup_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HelloRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreeterServer).SendBackup(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Greeter/SendBackup",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreeterServer).SendBackup(ctx, req.(*HelloRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Greeter_ServiceDesc is the grpc.ServiceDesc for Greeter service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -134,6 +168,10 @@ var Greeter_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CheckHeartbeat",
 			Handler:    _Greeter_CheckHeartbeat_Handler,
+		},
+		{
+			MethodName: "SendBackup",
+			Handler:    _Greeter_SendBackup_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
